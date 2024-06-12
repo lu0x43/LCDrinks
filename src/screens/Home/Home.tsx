280b9f0cf8b3869f7446f66dcd1c90ccd4e3e8e0
lucascaetano.dev <lucas_caetano123@hotmail.com>
2024-06-11 23:43:24 -0300

    install some dependencies, react dom, remix icon, svg, styled components, sqlite

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function HomePage(): React.JSX.Element {
+  return (
+    <>
+      <View>
+        <Text>Texto da Home</Text>
+      </View>
+    </>
+  );
+}
+
+export default HomePage;
