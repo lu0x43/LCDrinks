b95339ad853deea80149618cf0756f28c57b0151
lu0x43 <lucas_caetano123@hotmail.com>
2024-06-19 01:17:37 -0300

    add configs metro-config

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    assetExts: ['jpg', 'png', 'mp3'], // Adiciona novas extensões de asset suportadas
+  },
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'), // Configura o transformer para suportar SVGs
+  },
+  // Mais configurações podem ser adicionadas aqui
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
