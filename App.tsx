ccee0d2fb8d47fb1ac523104fe54da0d3f19b98c
lu0x43 <lucas_caetano123@hotmail.com>
2024-06-30 01:27:52 -0300

    add styles apptsx , position bottom of tabnavigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import { AppTabNav } from './src/routes';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <AppTabNav />
     </SafeAreaView>
   );
 }
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- src/screens/Menu/MenuScreen.tsx
+++ src/screens/Menu/MenuScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 function MenuScreen(): React.JSX.Element {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 20 }}>Menu de Drinks</Text>
+      <Text>Menu de Drinks</Text>
     </View>
   );
 }
